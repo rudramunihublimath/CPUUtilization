9b9196fc5b04c3cce279f3784d42bca478457343
rudramunihublimath <rudra.hublimath@gmail.com>
2020-04-10 17:43:31 +0530

    first chagne1

--- src/main/java/com/ex/CPUUtilize/CpuUtilizeApplication.java
+++ src/main/java/com/ex/CPUUtilize/CpuUtilizeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CpuUtilizeApplication {
-
+	// Comment
 	public static void main(String[] args) {
 		SpringApplication.run(CpuUtilizeApplication.class, args);
 	}
